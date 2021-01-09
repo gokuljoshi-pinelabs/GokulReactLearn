0f883d7c7403a72a89d694ac019b2248baa49944
gokuljoshi-pinelabs <68953285+gokuljoshi-pinelabs@users.noreply.github.com>
2021-01-09 10:27:53 +0530

    populate list

--- app1/src/components/userform/userform.js
+++ app1/src/components/userform/userform.js
@@ -6,27 +6,41 @@
 // }
 
 import React from "react";
+import './userform.css'
+
 export class Userform extends React.Component {
     // constructor(props){
     //     super(props);
     // }
-    constructor() {
+    constructor() {  //only one
         super();
         this.state = {
+            user :{
             fname: 'john',
-            copy: 'doe'
+            age: '30'
+         },
+         users:[{fname:'Ravi',age:20},{fname:'JOhn',age:60}]
         }
     }
 
-    clkBtn(params) {
-        console.log('Cons Save');
-        
+    clkBtn = (event) =>{
+        this.setState({
+            users:[...this.state.users,Object.assign({},this.state.user)]
+        }
+        )
     }
 
     print = (event)=>{
-        console.log(this.state.fname);
+        console.log(this.state.user.fname);
         this.setState({   //to rerender , call setstate
-            copy: this.state.fname
+            user:{age: this.state.user.fname}
+        })
+    }
+
+    handleEvent = (event) => {
+        this.setState({   //to rerender , call setstate
+          user:Object.assign(this.state.user,{[event.target.name]: event.target.value})
+          // user:{...this.state.user,[event.target.name]: event.target.value}
         })
     }
 
@@ -41,27 +55,35 @@ export class Userform extends React.Component {
             // this.state.fname = event.target.value}}
             // placeholder={this.props.label} style={{ backgroundColor: this.props.color }}></input>
             <div>
-                <input value={this.state.fname} onChange={(event) => {
-                    //console.log(this.state.fname);
-                    //this.state.fname = event.target.value}
-                    this.setState({   //to rerender , call setstate
-                        fname: event.target.value
-                    })
-                }
-                }
+                <input value={this.state.user.fname} 
+                // onChange={(event) => {   //no need to write it on every textbox
+                //     //console.log(this.state.fname);
+                //     //this.state.fname = event.target.value}
+                //     this.setState({   //to rerender , call setstate
+                //         fname: event.target.value
+                //     })
+                // }}
+                onChange={this.handleEvent} name='fname'
                     placeholder={this.props.label} style={{ backgroundColor: this.props.color }}></input>
 
-                <input value={this.state.copy}  onChange={(event) => {
-                    this.setState({   //to rerender , call setstate
-                        copy: event.target.value
-                    })
-                }}
-                placeholder='First Name Copy' style={{ backgroundColor: this.props.color }}></input>
+                <input value={this.state.user.age} name='age'  onChange={this.handleEvent}
+                placeholder='Age' style={{ backgroundColor: this.props.color }}></input>
                 
                 <button style={{ backgroundColor: this.props.color }} onClick={(event) => { console.log('inline click');console.log(this.state.fname); }}>Inline Save</button>
-                <button style={{ backgroundColor: this.props.color }} onClick={this.clkBtn}>Cons Save</button>
+                <button style={{ backgroundColor: this.props.color }} onClick={this.clkBtn}>Save to List</button>
                 <button onClick={this.print}>Print</button>
-
+                <table>
+                    <thead>
+                        <th>FirstName</th>
+                        <th>Age</th>
+                    </thead>
+                    <tbody>
+                        {this.state.users.map((user)=>{
+                            return <tr><td>{user.fname}</td><td>{user.age}</td></tr>
+                        })}
+                    </tbody>
+                    
+                </table>
             </div>
         )
 
