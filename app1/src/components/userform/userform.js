fbd57ae5ba2a6cef28ea55697f65054521b20282
gokuljoshi-pinelabs <68953285+gokuljoshi-pinelabs@users.noreply.github.com>
2021-02-06 08:33:07 +0530

    final code

--- app1/src/components/userform/userform.js
+++ app1/src/components/userform/userform.js
@@ -10,7 +10,9 @@ import { BackendService } from "../../backend-Service";
 import updateCountAction,{updateUserAction} from "../../redux-store/action";
 import './userform.css'
 import { connect } from "react-redux";
-
+import { event } from "jquery";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 class Userform extends React.Component {
 
@@ -28,34 +30,33 @@ class Userform extends React.Component {
             gender:'FeMale',
             role:'Programmer',
             sortOrder:true,
+            dob:'',
             skills:[]
          },
          users:[{fname:'Ravi',age:20,salary:100000},{fname:'JOhn',age:60,salary:200000}],
          roles:[]
         }
-                BackendService.getUsers(
-                    (response)=>{  //response callback
-                    this.setState({
-                        users:response
-                    }
-                    )
-            }
-                    ).fail((error)=>{
-                        alert("There is an issue in Get call");
-                    });
+              
+    }
+componentDidMount(){
+    this.getUsers();
+    this.getRoles();
+      
+}
 
-                    BackendService.getRoles(
-                        (response)=>{  //response callback
-                           
-                            this.setState({
-                                roles:response
-                            }
-                            )
-                            
-                        }                        
-                        ).fail((error)=>{
-                            alert("There is an issue in Get roles");
-                        });
+    getRoles = (event) =>{
+        BackendService.getRoles(
+            (response)=>{  //response callback
+               
+                this.setState({
+                    roles:response
+                }
+                )
+                
+            }                        
+            ).fail((error)=>{
+                alert("There is an issue in Get roles");
+            });
     }
 
     saveUser = (event) =>{
@@ -119,8 +120,9 @@ class Userform extends React.Component {
         })
     }
 
-    clkDelete = (index,userid)=>{
-
+    clkDelete = (index,userid,event)=>{
+       
+        event.stopPropagation();
         console.log('Delete click');
         
         const isDelete= window.confirm('Are you sure');
@@ -142,6 +144,7 @@ class Userform extends React.Component {
             //     {users:this.state.users}
             // )
         }
+      
 
         }
 
@@ -160,6 +163,13 @@ class Userform extends React.Component {
                 users: this.state.users,
                 sortOrder:order
               })
+        }
+
+        handleDateChange= function(){
+                console.log(arguments);
+                this.setState({  
+                         user:{...this.state.user,[this.state.user.dob]:arguments[0]}
+                })
         }
        
     handleEvent = (event) => {
@@ -216,14 +226,14 @@ class Userform extends React.Component {
                 onChange={this.handleEvent} name='fname'
                     placeholder={this.props.label} style={{ backgroundColor: this.props.color }}></input>
 
-                <input value={userModel.age} name='age'  onChange={this.handleEvent}
+                <input value={userModel.age} name='age' type='number' min='1' max='150' onChange={this.handleEvent}
                 placeholder='Age' style={{ backgroundColor: this.props.color }}></input>
 
-                <input value={userModel.salary} name='salary'  onChange={this.handleEvent}
+                <input value={userModel.salary} name='salary' min='100'  type='number' onChange={this.handleEvent}
                 placeholder='salary' style={{ backgroundColor: this.props.color }}></input>
 
                 <input type="radio" value="Male" onChange={this.handleEvent} name="gender"/>Male
-                <input type="radio" checked="true" value="Female" onChange={this.handleEvent} name="gender"/>FeMale
+                <input type="radio" checked={true} value="Female" onChange={this.handleEvent} name="gender"/>FeMale
 
                 {this.state.roles.map((role,index)=>{
                             return <span><input type="radio" value={role} onChange={this.handleEvent} name="role"/>{role}</span>
@@ -232,6 +242,9 @@ class Userform extends React.Component {
                 <input name='skills' type="checkbox" onChange={this.handleEvent} value="React" />React  
                 <input name='skills'  type="checkbox" onChange={this.handleEvent} value="ReactNative" />React Native  
                 <input name='skills' onChange={this.handleEvent} value="Javascript"  type="checkbox"/>Javascript      
+                
+                DOB:<DatePicker selected={this.state.user.dob} onChange={this.handleDateChange.bind(this)}></DatePicker>
+                
                 </div>
                 <button style={{ backgroundColor: this.props.color }} onClick={(event) => { console.log('inline click');console.log(userModel.fname); }}>Inline Save</button>
                 <button style={{ backgroundColor: this.props.color }} onClick={this.saveUser}>Save User</button>
@@ -239,6 +252,7 @@ class Userform extends React.Component {
                 <button onClick={this.getUsers}>Get Users</button>
                 <table>
                     <thead>
+                        <tr>
                     <th>Index</th>
                     <th>ID</th>
                         <th>FirstName<div><input onBlur={this.getUsersByName}/></div></th>
@@ -248,6 +262,7 @@ class Userform extends React.Component {
                         <th>Role</th>
                         <th>skills</th>
                         <th>Delete</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {this.state.users.map((user,index)=>{
--- app1/src/index.js
+++ app1/src/index.js
@@ -13,7 +13,7 @@ import reducers from './redux-store/reducers';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-const initialState = {count:5,user:{}}
+const initialState = {}
 const AllReducers = combineReducers({data:reducers});
 const mystore = createStore(AllReducers,initialState,compose(applyMiddleware(thunk),
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
